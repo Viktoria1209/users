e670bafca2af1b59a6814ec2d4603c47b7912de0
v.boyarko <v.boyarko@abtollc.com>
2025-10-07 17:00:10 +0300

    added project skeleton; prettier settings and ESLint

---
+++ usersApp/src/app/users/components/users-table/users-table.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-users-table",
+	standalone: true,
+	imports: [],
+	templateUrl: "./users-table.html",
+	styleUrl: "./users-table.scss",
+})
+export class UsersTable {}
